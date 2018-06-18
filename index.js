51cc8496b578aff2125bfd3519f4435ca72fa3bb
“[Ryan <[ryanmiller12@bellsouth.net]>
2018-06-18 12:06:35 -0400

    add handlebars and templates

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-//Init
+//Init Express.js
 const express = require("express");
 const app = express();
+const expressHbs = require("express-handlebars");
+
+app.engine(".hbs", expressHbs({defaultLayout: "layout", extname: ".hbs"}));
+app.set("view engine", ".hbs");
+
+//Init Contacts Data
+const contacts = require("./contacts.json");
 
 
 //Application Function
 //User Welcome Message
 app.get("/", function(request, response) {
-    response.send("Hello, welcome to ContactsApp!");
+    response.render("home", {
+        message: "Welcome to the ContactsApp home page!",
+        footerContent: "Footer Stuffs"
+    });
 });
 
 
 //Contacts List
+//User Data
 app.get("/contacts", function(request, response) {
-    response.send("Welcome to the listing page!");
+    response.send(contacts.users);
+});
+
+//User IDs
+app.get("/contacts/:id", function(request, response) {
+    let id = request.params.id;
+    let contact = contacts.users.find(function(user) {
+        return user.id === id;
+    });
+    if (!contact) {
+        contact = "User does not exist!";
+    }
+    else {response.send(contact)};
 });
 
 //Contacts Details
